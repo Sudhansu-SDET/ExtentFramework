20e6ac00b74868ccb301bb093249564ed7150841
sudhansu-selenium <ricky.patro@gmail.com>
2021-02-08 17:43:22 +0530

    extentreport code

---
+++ src/test/java/qaclickacademy/ExtentReports/javascriptpoc.java
@@ -0,0 +1,22 @@
+package qaclickacademy.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class javascriptpoc {
+	
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\sudpatro\\Desktop\\MYDOCS\\SeleniumLearning\\mydriver85\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+}
